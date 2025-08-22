2f87ba18b187806355cde90e8335f183724f5eaf
Emanuel Sanchez <emanuelsancheztb@gmail.com>
2025-08-22 03:50:46 -0300

    Integrando dependencias de Gulp para JS y agregando modernizr.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,20 @@ const { src, dest, watch, parallel } = require('gulp');
 
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('autoprefixer');
-const postcss = require('gulp-postcss');
+const postcss = require('gulp-postcss')
+const sourcemaps = require('gulp-sourcemaps')
 const cssnano = require('cssnano');
-// const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser-js');
+const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
-const cache = require('gulp-cache');
 const notify = require('gulp-notify');
-const sourcemaps = require('gulp-sourcemaps');
+const cache = require('gulp-cache');
 const webp = require('gulp-webp');
-// const clean = require('gulp-clean');
 
 const paths = {
     scss: 'src/scss/**/*.scss',
-    // js: 'src/js/**/*.js',
+    js: 'src/js/**/*.js',
     imagenes: 'src/resources/img/**/*'
 };
 
@@ -27,25 +28,35 @@ function css() {
         .pipe( dest('./build/css') );
 };
 
+function javascript() {
+    return src( paths.js )
+      .pipe( sourcemaps.init() )
+      .pipe( concat('bundle.js') )
+      .pipe( terser() )
+      .pipe( sourcemaps.write('.') )
+      .pipe( rename( { suffix: '.min' } ) )
+      .pipe( dest('./build/js') )
+};
+
 function imagenes() {
     return src( paths.imagenes )
         .pipe( cache( imagemin( {optimizationLevel: 3} ) ) )
-        .pipe( dest('build/img') )
+        .pipe( dest('build/resources/img') )
         .pipe( notify( {message: 'Imagen Completa'} ) )
 };
 
 function versionWebp() {
     return src( paths.imagenes )
         .pipe( webp() )
-        .pipe( dest('build/img') )
+        .pipe( dest('build/resources/img') )
         .pipe ( notify( {message: 'Imagen Completada'} ) )
 };
 
 function watchArchivos() {
     watch( paths.scss, css );
-    // watch( paths.js, javascript );
+    watch( paths.js, javascript );
     watch ( paths.imagenes, imagenes ),
     watch( paths.imagenes, versionWebp );
 };
 
-exports.default = parallel( css, imagenes, versionWebp, watchArchivos );
+exports.default = parallel( css, javascript, imagenes, versionWebp, watchArchivos );
