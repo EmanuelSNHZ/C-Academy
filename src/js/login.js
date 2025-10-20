fe3f08a6e231319eb0eacc31b43041e1a9e1b06c
Emanuel Sanchez <emanuelsancheztb@gmail.com>
2025-10-20 18:19:31 -0300

    Finalizando login

---
+++ src/js/login.js
@@ -0,0 +1,20 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+    
+    viewPassword();
+} );
+
+function viewPassword() {
+    
+    const togglePassword = document.querySelector("#togglePassword");
+    const passwordInput = document.querySelector("#password");
+
+    togglePassword.addEventListener("click", () => {
+        const isPassword = passwordInput.type === "password";
+        passwordInput.type = isPassword ? "text" : "password";
+
+        // Cambiar de icono con el click
+        togglePassword.src = isPassword
+            ? "build/resources/img/eye-closed.svg"
+            : "build/resources/img/eye.svg";
+    })
+}
